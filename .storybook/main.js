7780a48cb8682cccc56dfeec11912dd7311982a9
Luiz Henrique <luizhjramos@outlook.com>
2022-06-21 11:50:54 -0300

    feat(stories): Button documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.mdx","../src/**/**/*.stories.@(js|jsx|ts|tsx)" , "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -8,5 +8,15 @@ module.exports = {
   framework: "@storybook/web-components",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    // find web-components rule for extra transpilation
+    const webComponentsRule = config.module.rules.find(
+      (rule) => rule.use && rule.use.options && rule.use.options.babelrc === false
+    );
+    // add your own `my-library`
+    webComponentsRule.test.push(new RegExp(`node_modules(\\/|\\\\)my-library(.*)\\.js$`));
+
+    return config;
   },
 };
